02b50d206ea6a434c70bb1d0f884acd2a2fadb78
José Burgos Guntín <101651981+joseburgosguntin@users.noreply.github.com>
2024-02-06 17:01:32 -0400

    :heavy_minus_sign: markdown-preview.nvim

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -81,18 +81,5 @@ local plugins = {
       require("nvim-ts-autotag").setup()
     end,
   },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = {
-      "MarkdownPreviewToggle",
-      "MarkdownPreview",
-      "MarkdownPreviewStop",
-    },
-    build = "cd app && yarn install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  },
 }
 return plugins
